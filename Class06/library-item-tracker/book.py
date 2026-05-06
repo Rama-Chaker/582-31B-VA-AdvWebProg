d9e994181cab0c6aa3f89580d2c3e9194fdde31e
Rama-Chaker <rama.chaker1@gmail.com>
2026-05-06 11:05:21 -0400

    Added a counter and its function in book.py and tested it in main.py

--- Class06/library-item-tracker/book.py
+++ Class06/library-item-tracker/book.py
@@ -1,29 +1,34 @@
 class Book:
     library_name = "Central Library"
-    def __init__(self, title, author,available):
+    count = 0
+
+    def __init__(self, title, author, available):
         self.title = title
         self.author = author
         self.available = available
-    
+        Book.count += 1
+
+    @classmethod
+    def show_count(cls):
+        print(f"Total number of books: {cls.count}")
     @classmethod
     def change_library_name(cls, new_name):
         cls.library_name = new_name
-    
+
     @staticmethod
     def is_valid_title(title):
-       if(len(title) > 0):
-           return print("Valid title")
-       else:
-           return print("Invalid title")
-    
-    
+        if len(title) > 0:
+            return print("Valid title")
+        else:
+            return print("Invalid title")
+
     def borrow(self):
         if self.available:
             self.available = False
             print(f"You have borrowed '{self.title}' by {self.author}.")
         else:
             print(f"Sorry, '{self.title}' is currently not available.")
-     
+
     def return_book(self):
         if self.available == False:
             self.available = True
@@ -32,10 +37,10 @@ class Book:
             print(f"'{self.title}' was not borrowed.")
 
     def display_info(self):
-       print(f"""
-Book Report
-----------------
-Title: {self.title}
-Author: {self.author}
-Available: {'In Stock' if self.available else 'Out of Stock'}
-""")
+        print(f"""
+                Book Report
+                ----------------
+                Title: {self.title}
+                Author: {self.author}
+                Available: {'In Stock' if self.available else 'Out of Stock'}
+                """)
