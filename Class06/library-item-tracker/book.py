a27d619b8aa4e5c81393d8583c42ddd8f21e664a
Rama-Chaker <rama.chaker1@gmail.com>
2026-05-06 10:21:19 -0400

    Added code to book.py and initialized 2 book objects in main.py

---
+++ Class06/library-item-tracker/book.py
@@ -0,0 +1,8 @@
+class Book:
+    title = "Unknown Title"
+    author = "Unknown Author"
+    available = True
+    def __init__(self, title, author,available):
+        self.title = title
+        self.author = author
+        self.available = available
