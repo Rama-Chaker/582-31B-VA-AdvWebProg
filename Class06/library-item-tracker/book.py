aaeb031cff7c2d998c9c19a444d54134449ce7c1
Rama-Chaker <rama.chaker1@gmail.com>
2026-05-06 10:27:11 -0400

    Added 3 new methods to book.py

--- Class06/library-item-tracker/book.py
+++ Class06/library-item-tracker/book.py
@@ -6,3 +6,20 @@ class Book:
         self.title = title
         self.author = author
         self.available = available
+    
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"You have borrowed '{self.title}' by {self.author}.")
+        else:
+            print(f"Sorry, '{self.title}' is currently not available.")
+     
+    def return_book(self):
+        if self.available == False:
+            self.available = True
+            print(f"You have returned '{self.title}' by {self.author}.")
+        else:
+            print(f"'{self.title}' was not borrowed.")
+
+    def display_info(self):
+        print(f"Title: {self.title}, Author: {self.author}, Available: {self.available}")
--- Class06/library-item-tracker/main.py
+++ Class06/library-item-tracker/main.py
@@ -3,3 +3,4 @@ book1 = Book("1984", "George Orwell", True)
 book2 = Book("To Kill a Mockingbird", "Harper Lee", False)
 print(f"Title: {book1.title}, Author: {book1.author}, Available: {book1.available}")
 print(f"Title: {book2.title}, Author: {book2.author}, Available: {book2.available}")
+
