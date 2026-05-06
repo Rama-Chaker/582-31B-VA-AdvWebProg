b4da099d10ba46a39ba2666a308944288a837adf
Rama-Chaker <rama.chaker1@gmail.com>
2026-05-06 11:02:50 -0400

    Updated main.py: Modified the display of the library name changings.

--- Class06/library-item-tracker/main.py
+++ Class06/library-item-tracker/main.py
@@ -1,17 +1,17 @@
 from book import Book
+
 book1 = Book("1984", "George Orwell", True)
 book2 = Book("To Kill a Mockingbird", "Harper Lee", False)
 print(f"Title: {book1.title}, Author: {book1.author}, Available: {book1.available}")
 print(f"Title: {book2.title}, Author: {book2.author}, Available: {book2.available}")
-book1.change_library_name("Downtown Library")
-print(f"Library Name: {book1.library_name}")
-Book.is_valid_title("The Great Gatsby")
-Book.is_valid_title("")
-
-
 book1.borrow()
 book2.borrow()
 book1.return_book()
 book2.return_book()
 book1.display_info()
 book2.display_info()
+print(f"Library Name before changing it for the 1st book: {Book.library_name}")
+book1.change_library_name("Downtown Library")
+print(f"Library Name after the change: {book1.library_name}")
+Book.is_valid_title("The Great Gatsby")
+Book.is_valid_title("")
