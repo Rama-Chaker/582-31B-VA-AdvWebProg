c6fbc1dd377d4124709ec33ddacc7e9d00911828
Rama-Chaker <rama.chaker1@gmail.com>
2026-06-09 10:19:34 -0400

    Added the Bonus A, and tests

--- Class15/movie_show.py
+++ Class15/movie_show.py
@@ -1,4 +1,5 @@
 from show_status import ShowStatus
+from user import Customer
 from show_status import MAX_TICKETS_PER_BOOKING
 class MovieShow:
     def __init__(self, title, capacity, booked_seats, status):
@@ -55,7 +56,19 @@ class MovieShow:
         self.status = ShowStatus.CANCELLED
     def display_info(self):
         print(f"Title: {self.__title} | Capacity: {self.__capacity} | Booked Seats: {self.__booked_seats} | Status: {self.__status.value}")
+    #Bonus A
+    def reopen_show(self):
+     if self.__status == ShowStatus.CANCELLED:
+        raise ShowCancelledError(
+            "Cancelled shows cannot be reopened."
+        )
 
+     if self.__status != ShowStatus.SOLD_OUT:
+        raise InvalidStatusError(
+            "Only sold-out shows can be reopened."
+        )
+
+     self.__status = ShowStatus.OPEN
 class InvalidBookingError(Exception):
     pass
 
@@ -65,3 +78,62 @@ class ShowCancelledError(Exception):
     pass
 class InvalidStatusError(Exception):
     pass
+
+#Tests
+print("Testing MovieShow class : Happy Path")
+customer1 = Customer(
+    "John Doe",
+    "john@email.com",
+    101
+)
+
+show = MovieShow(
+    "Avengers",
+    100,
+    0,
+    ShowStatus.OPEN
+)
+
+show.display_info()
+
+try:
+    print(show.book_tickets(customer1, 5))
+except Exception as e:
+    print(e)
+
+show.display_info()
+
+print("Invalid Booking Test")
+try:
+    show.book_tickets(customer1, 1000)
+except InvalidBookingError as e:
+    print("Booking Error:", e)
+try:
+    sold_out_show = MovieShow(
+        "Batman",
+        10,
+        10,
+        ShowStatus.SOLD_OUT
+    )
+
+    sold_out_show.book_tickets(customer1, 1)
+
+except ShowSoldOutError as e:
+    print("Sold Out Error:", e)
+try:
+    cancelled_show = MovieShow(
+        "Superman",
+        50,
+        0,
+        ShowStatus.CANCELLED
+    )
+
+    cancelled_show.book_tickets(customer1, 1)
+except ShowCancelledError as e:
+    print("Cancelled Show Error:", e)
+
+try:
+    show.status = "OPEN"
+
+except InvalidStatusError as e:
+    print("Status Error:", e) 
